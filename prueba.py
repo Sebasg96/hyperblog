2d91de855b3a64658d869e3034f0f0f7134d11e5
Sebas Galindo <sebastiangalindo09@gmail.com>
2023-06-09 19:46:14 -0500

    commit antes de enviar todo a github

--- prueba.py
+++ prueba.py
@@ -1,4 +1,5 @@
-def is_armstrong_number(number):
+from collections  import Counter
+'''def is_armstrong_number(number):
     sum_value = 0
     number_list = [int(x) for x in str(number)]
     for element in number_list:
@@ -6,4 +7,16 @@ def is_armstrong_number(number):
     return sum_value == number
 
     
-print(is_armstrong_number(153))
+print(is_armstrong_number(153))'''
+
+dice_1 = [2,4,4,4,3]
+
+sort_dice = Counter(dice_1).most_common(1)
+
+def four_of_a_kind(dice):
+    sort_dice = Counter(dice_1).most_common(1)
+    return sort_dice[0][0]*4 if sort_dice[0][1] >= 4 else 0
+
+
+print(four_of_a_kind(dice_1))
+
